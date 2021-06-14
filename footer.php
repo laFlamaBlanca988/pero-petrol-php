69cef835901228bc95cda0e8c77f683811c09a88
laFlamaBlanca988 <laflamablanca988@hotmail.com>
2021-06-14 10:31:20 +0200

    Initial commit

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
 <footer class="footer container-fluid">
-    <!-- Copyright -->
     <div class="text-center text-light p-3">
-        © 2020 Copyright:
-        <a class="text-light">ombreNegro</a>
+        © 2021 Copyright:
+        <a class="text-light">OmbreNegro</a>
     </div>
-    <!-- Copyright -->
 </footer>
